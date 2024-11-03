78efe947b64bca341702aa637441757b74daf29b
tawseefnabi <tawseefnabi9@gmail.com>
2024-11-03 17:29:38 +0530

    Initial commit

---
+++ test/cdk-ts-apigw.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTsApigw from '../lib/cdk-ts-apigw-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ts-apigw-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTsApigw.CdkTsApigwStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
